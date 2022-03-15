c3a87451ea8e28674ac85830ac0abcf850746a33
Qinther <86771611+Qinther@users.noreply.github.com>
2022-03-15 16:35:07 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,29 +5,28 @@
 #include "fft.h"
 
 
-int adcbuff[128] = {};
+int adcbuff[128] = {}; // 采样结果数组
 float vpp;
 float thd;
+
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;
-    InitADC();
-    InitSystemClock();
+    InitADC(); // 初始化ADC10采样
+    InitSystemClock(); // 初始化系统时钟
     P1DIR |= BIT1+BIT2+BIT3+BIT4;
-    P2DIR |= BIT0+BIT1+BIT2+BIT3+BIT4;
-
+    P2DIR |= BIT0+BIT1+BIT2+BIT3+BIT4; // LCD引脚设置
+    initial_lcd(); // 初始化LCD
     int i;
     while(1)
     {
-        for(i=0;i<128;i++) StartADCConvert();
-        get_vpp();
-        _nop();
+        for(i=0;i<128;i++) StartADCConvert(); // 开始采样
+        get_vpp(); // 计算峰峰值
         FFTR_SEQ();
-        FFTR();
-        THD();
-        initial_lcd();
-        clear_screen();
+        FFTR(); // 傅里叶变换结束
+        THD(); // 计算失真度
+        clear_screen(); // 清屏
         display_GB2312_string(5,1,"THD:");
         display_float(5,32,thd);
         display_GB2312_string(5,75,"%");
@@ -35,7 +34,6 @@ int main(void)
         display_float(7,32,vpp);
         display_GB2312_string(7,75,"V");
         delay(20000);
-        _nop();
     }
 //  return 0;
 }
